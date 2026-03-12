02b9158da07cf1cb14c0f30773bab61ad9ec78b4
pste <pirraste@gmail.com>
2026-03-12 15:44:08 +0100

    [css] cleared App

--- src/App.vue
+++ src/App.vue
@@ -7,19 +7,11 @@ import CameraView from './views/CameraView.vue';
 </template>
 
 <style scoped>
-  html, body, main, section{
-    height: 100%;
-    width: 100%;
-  }
-  
-  html{
-    position: fixed;
-  }
-  
-  body{
-    font-family: 'Lato', sans-serif;
-    -webkit-tap-highlight-color: transparent;
-    -webkit-touch-callout: none;
-    -webkit-user-select: none;
-  }
-  </style>
+html {
+  position: fixed;
+}
+
+body {
+  overflow: hidden;
+}
+</style>
