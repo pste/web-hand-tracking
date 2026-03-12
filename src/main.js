e3e0a498addf300755a1c3504887f2be18b68f11
pste <pirraste@gmail.com>
2026-03-12 15:43:34 +0100

    [css] reset.css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import registerPlugins from '@/plugins'
 
+import "./assets/reset.css";
+
 const app = createApp(App)
 registerPlugins(app)
 app.mount('#app')
